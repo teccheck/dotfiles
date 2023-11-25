650963d6a40a6e964a3f02413bbdb8242e47e976
TecCheck <mcteccy@protonmail.com>
2023-11-25 15:33:20 +0100

    Refactor fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,30 +3,15 @@ if status is-interactive
 end
 
 function fish_prompt -d "Write out the prompt"
-    # This is a simple prompt. It looks like
-    # alfa@nobby /path/to/dir $
-    # with the path shortened and colored
-    # and a "#" instead of a "$" when run as root.
     set -l symbol '$'
     set -l color $fish_color_cwd
+
     if fish_is_root_user
         set symbol '#'
         set -q fish_color_cwd_root
         and set color $fish_color_cwd_root
     end
 
-    set_color $color
-    echo -n $USER@$hostname
-
-    set_color normal
-    echo -n ':'
-
-    set_color brblue
-    echo -n (prompt_pwd)
-    echo -n " "
-    echo -n $symbol
-
-    set_color normal
-    echo -n " "
+    echo -n (set_color $color)$USER@$hostname(set_color normal):(set_color brblue; prompt_pwd;) $symbol (set_color normal)
 
 end
