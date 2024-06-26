413f8a99934ec6c36aab4cfe2fc5635e132b0e2c
Teccheck <mcteccy@protonmail.com>
2024-06-26 16:00:45 +0200

    Add fish function to pull git repos recursively

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,10 +4,14 @@ end
 
 set turbofish false
 
-function fish_prompt -d "Write out the prompt"    
+function fish_prompt -d "Write out the prompt"
     if $turbofish
         echo -n (set_color -o brblue)"::<"(set_color brblue; prompt_pwd;)(set_color bryellow; fish_vcs_prompt)(set_color -o brblue)">" (set_color normal)
     else
         echo -n (set_color brblue; prompt_pwd;)(set_color bryellow; fish_vcs_prompt)(set_color -o brblue) ">" (set_color normal)
     end
+end
+
+function gplr -d "Pull git repos recursively (with rebase)"
+    find -maxdepth 3 -name .git -type d -printf '\n'(set_color -o brblue)'%h:'(set_color normal)'\n' -execdir git pull --rebase ";"
 end
