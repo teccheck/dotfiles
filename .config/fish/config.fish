fc8578c60b0dfc91d276f49228f27f2de92b451a
TecCheck <mcteccy@protonmail.com>
2024-07-23 18:54:56 +0200

    Add colors command to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,10 +8,22 @@ function fish_prompt -d "Write out the prompt"
     if $turbofish
         echo -n (set_color -o brblue)"::<"(set_color brblue; prompt_pwd;)(set_color bryellow; fish_vcs_prompt)(set_color -o brblue)">" (set_color normal)
     else
-        echo -n (set_color brblue; prompt_pwd;)(set_color bryellow; fish_vcs_prompt)(set_color -o brblue) ">" (set_color normal)
+        echo -n (set_color brblue; prompt_pwd;)(set_color brpurple; fish_vcs_prompt)(set_color -o brblue) ">" (set_color normal)
     end
 end
 
 function gplr -d "Pull git repos recursively (with rebase)"
     find -maxdepth 3 -name .git -type d -printf '\n'(set_color -o brblue)'%h:'(set_color normal)'\n' -execdir git pull --rebase ";"
+end
+
+function colors -d "Show colors with their names in fish"
+    for color in black red green yellow blue purple cyan white
+        echo -n (set_color -b $color)"  "$color"  "(set_color normal)
+    end
+    echo ""
+
+    for color in brblack brred brgreen bryellow brblue brpurple brcyan brwhite
+        echo -n (set_color -b $color) $color (set_color normal)
+    end
+    echo ""
 end
